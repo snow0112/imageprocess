f19c87cdea75fcaa31777488b97bf94a4ce3278c
snow <hsuehweh@usc.edu>
2020-02-06 09:14:33 -0800

    transformation

--- ImageDisplay.java
+++ ImageDisplay.java
@@ -80,7 +80,7 @@ public class ImageDisplay {
 
 	}
 	
-	private void Rotate(float angle, int width2, int height2, BufferedImage old_img, BufferedImage new_img){
+	private void Rotate(float angle, int width, int height, int width2, int height2, BufferedImage old_img, BufferedImage new_img){
 		int cent_new_x = width2/2;
 		int cent_new_y = height2/2;
 		int cent_x = width/2;
@@ -111,6 +111,45 @@ public class ImageDisplay {
 		return (int) ans;
 	}
 
+	private BufferedImage Transformation(int width, int height, float scale, float rotation, BufferedImage imgOne){
+		// Scale the image by scale
+		if (scale != 1.0) {
+			int width2 = Math.round(width*scale);
+			int height2 = Math.round(height*scale);
+			//System.out.println("Scale to: " + width2 +","+ height2);
+			imgTwo = new BufferedImage(width2, height2,  BufferedImage.TYPE_INT_RGB);
+			Scale(scale,imgOne, imgTwo);
+			imgOne = imgTwo;
+			width = width2;
+			height = height2;
+		}
+
+		// Rotate the image
+		while(rotation < 0) rotation += 360;
+		while(rotation >= 360) rotation -= 360;
+		if (rotation != 0){
+			// calculate new size
+			int left, right, up, low;
+			left = 0; right = 0; up = 0; low = 0;
+			int[][] corners = { {width,0},{0,height},{width,height} };
+			for(int c = 0; c <3; c++){
+				left = Math.min(left, rotate_x(rotation, corners[c][0], corners[c][1]));
+				right = Math.max(right, rotate_x(rotation, corners[c][0], corners[c][1]));
+				up = Math.min(up, rotate_y(rotation, corners[c][0], corners[c][1]));
+				low = Math.max(low, rotate_y(rotation, corners[c][0], corners[c][1]));
+			}
+			int width2 = right - left;
+			int height2 = low - up;
+			//System.out.println("The third parameter (rotation) was: " + rotation + "; new size:" + width2 +","+ height2 );
+			imgTwo = new BufferedImage(width2, height2,  BufferedImage.TYPE_INT_RGB);
+			Rotate(rotation, width, height, width2, height2, imgOne, imgTwo);
+			imgOne = imgTwo;
+			width = width2;
+			height = height2;
+		}
+		return imgTwo;
+	}
+	
 	private void LPF(BufferedImage old_img, BufferedImage new_img){
 		for(int y = 0; y < height; y++){
 			for(int x = 0; x < width; x++){
@@ -163,41 +202,6 @@ public class ImageDisplay {
 		imgOne = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		readImageRGB(width, height, args[0], imgOne);
 
-		// Scale the image by scale
-		if (scale != 1.0) {
-			int width2 = Math.round(width*scale);
-			int height2 = Math.round(height*scale);
-			//System.out.println("Scale to: " + width2 +","+ height2);
-			imgTwo = new BufferedImage(width2, height2,  BufferedImage.TYPE_INT_RGB);
-			Scale(scale,imgOne, imgTwo);
-			imgOne = imgTwo;
-			width = width2;
-			height = height2;
-		}
-
-		// Rotate the image
-		while(rotation < 0) rotation += 360;
-		while(rotation >= 360) rotation -= 360;
-		if (rotation != 0){
-			// calculate new size
-			int left, right, up, low;
-			left = 0; right = 0; up = 0; low = 0;
-			int[][] corners = { {width,0},{0,height},{width,height} };
-			for(int c = 0; c <3; c++){
-				left = Math.min(left, rotate_x(rotation, corners[c][0], corners[c][1]));
-				right = Math.max(right, rotate_x(rotation, corners[c][0], corners[c][1]));
-				up = Math.min(up, rotate_y(rotation, corners[c][0], corners[c][1]));
-				low = Math.max(low, rotate_y(rotation, corners[c][0], corners[c][1]));
-			}
-			int width2 = right - left;
-			int height2 = low - up;
-			//System.out.println("The third parameter (rotation) was: " + rotation + "; new size:" + width2 +","+ height2 );
-			imgTwo = new BufferedImage(width2, height2,  BufferedImage.TYPE_INT_RGB);
-			Rotate(rotation, width2, height2, imgOne, imgTwo);
-			imgOne = imgTwo;
-			width = width2;
-			height = height2;
-		}
 
 		// Aliasing
 		if (alising == 1){
@@ -206,12 +210,15 @@ public class ImageDisplay {
 			imgOne = imgTwo;
 		}
 
+		// Transformation
+		imgTwo = Transformation(width, height, scale, rotation, imgOne);
+
 		// Use label to display the image
 		frame = new JFrame();
 		GridBagLayout gLayout = new GridBagLayout();
 		frame.getContentPane().setLayout(gLayout);
 
-		lbIm1 = new JLabel(new ImageIcon(imgOne));
+		lbIm1 = new JLabel(new ImageIcon(imgTwo));
 
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
